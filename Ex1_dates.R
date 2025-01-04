c2f6606531745d1c8e67845c75b21d21a79cff67
dcrivers515 <drew@chronicle-r.com>
2025-01-04 12:06:26 -0600

    commit on 1/4

--- Ex1_dates.R
+++ Ex1_dates.R
@@ -4,9 +4,11 @@ lubridate::today()
 
 library(lubridate)
 month(today())
+month(today(), label = TRUE)
 date()
 today()
-d <- today()
+
+d <- "2025-01-04"
 d2 <- ymd(d)
 
 class(d)
@@ -14,5 +16,11 @@ class(d2)
 
 Sys.Date()
 
-# Sys.sleep(20)
+# Sys.sleep(20) # puts R session to sleep for 20 seconds
+ # important to put system to sleep when web scraping or site 
+ # could see you as a bot and block your ip address
 
+
+
+
+ 
