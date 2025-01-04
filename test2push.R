78ca10ffe9e15f43ea598c0536727749f8fe79b1
dcrivers515 <drew@chronicle-r.com>
2025-01-04 11:53:11 -0600

    deleting 2 files

--- test2push.R
+++
@@ -1,7 +0,0 @@
-# this another test of shitty github
-
-date()
-
-install.packages("usethis")
-library(usethis)
-create_github_token()
--- testgitpush.R
+++
@@ -1,5 +0,0 @@
-## this is a test script 
-
-git = "sucks"
-
-# where is this commit going to??????
